6d64a577e1f0cd0e3583fe2b2af1ec9715bbd2f5
Dávio Carvalho <129198119+Davio27@users.noreply.github.com>
2024-04-27 12:18:26 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './MainScreen';
 import QRScreen from './QRScreen';
-import LeitorScreen from './LeitorScreen'
-import CrudScreen from './CrudScreen'
-import LoginScreen from './LoginScreen'
+import LeitorScreen from './LeitorScreen';
+import CrudScreen from './CrudScreen';
+import LoginScreen from './LoginScreen';
 
+// Criação do Stack Navigator
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="MainScreen">
         <Stack.Screen 
           name="MainScreen" 
           component={MainScreen} 
@@ -43,4 +44,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App; 
